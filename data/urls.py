e993b93aa8ada1f02de9a82ef7818400ccd35b34
alireza hajli <hajlialireza@gmail.com>
2022-08-24 22:10:36 +0430

    Update urls.py

--- data/urls.py
+++ data/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt
 from . import views
 
 router = routers.DefaultRouter()
@@ -11,4 +12,6 @@ router.register('history-ratio', views.history_ratio)
 app_name = "data"
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('token/', jwt.TokenObtainPairView.as_view(), name='token'),
+	path('token-refresh/', jwt.TokenRefreshView.as_view(), name='refresh_token'),
 ]
